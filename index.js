4af1549ab2763abea0d5ddbedd7cf87137b3585a
LuisC <lullero98@gmail.com>
2021-02-02 13:45:34 -0600

    Estructura inicial

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const app = express()
+
+//config File
+const config = require('./config/config');
+
+app.listen(3000,(error){
+    if(error) throw error;
+
+    console.log(`Running server at port ${config.PORT}` )
+
+})
